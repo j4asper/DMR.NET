3154850abec2eb875d61de24fd4be05fc566c590
Jasper <jasper@jazper.dk>
2025-03-14 14:41:24 +0100

    Added ServiceCollection and HostBuilder extensions

---
+++ DMR.NET/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace DMR.NET.Extensions;
+
+public static class HostBuilderExtensions
+{
+    public static IHostBuilder UseDmrDotNet(this IHostBuilder hostBuilder)
+    {
+        hostBuilder.ConfigureServices((_, services) => services.AddDmrDotNetServices());
+        
+        return hostBuilder;
+    }
+}
---
+++ DMR.NET/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using DMR.NET.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DMR.NET.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDmrDotNetServices(this IServiceCollection services)
+    {
+        services
+            .AddSingleton<IDmrFtpService, DmrFtpService>()
+            .AddSingleton<IDmrDeserializerService, DmrDeserializerService>();
+        
+        return services;
+    }
+}
